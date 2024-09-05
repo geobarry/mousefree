a4a378b1722f746740a1659cf861c49193ab627c
Barry J Kronenfeld <bjkronenfeld@eiu.edu>
2024-09-05 17:46:46 -0500

    did something to text selection not sure what

--- windows accessibility/text selection/text selection.py
+++ windows accessibility/text selection/text selection.py
@@ -5,12 +5,14 @@ from enum import Enum
 ax_dir_options = ["NEXT","PREVIOUS","ANY"]
 ax_unit_options = ["Character","Format","Word","Line","Paragraph","Page","Document"]
 
+mod = Module()
+
 @mod.action_class
 class Actions:
     def find_target(target_regex: str, 
-                    scope_dir: str = "NEXT",
+                    scope_dir: str = "ANY",
                     scope_unit: str = "Line",
-                    scope_unit_count: int = 15):
+                    scope_unit_count: int = 7):
         """Returns a text range corresponding to the given target"""
         print(f'scope_dir: {scope_dir}')
         print(f'scope_unit: {scope_unit}')
@@ -26,4 +28,9 @@ class Actions:
             print("Error in function find_target: focused element does not have text pattern")
             return 
         # Get scope as a text range
-        
+        cur_range = el.text_pattern.selection[0].clone()
+        cur_range.expand_to_enclosing_unit(scope_unit)
+        cur_range.move_endpoint_by_unit("End",scope_unit,scope_unit_count)
+        cur_range.move_endpoint_by_unit("Start",scope_unit,-1*scope_unit_count)
+        # get rid of this later once everything is working:
+        cur_range.select()
