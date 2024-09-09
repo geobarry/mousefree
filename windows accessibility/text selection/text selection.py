c8808699ce52e70fd9f3f7ee6e640ced8f41e4b4
Barry Kronenfeld <barrykronenfeld@gmail.com>
2024-09-08 20:21:36 -0500

    created select extent and go commands with windows accessibility text selection

--- windows accessibility/text selection/text selection.py
+++ windows accessibility/text selection/text selection.py
@@ -1,49 +1,85 @@
 from talon.windows import ax as ax,ui as winui
 from talon import app, Context,Module,actions, ctrl,ui
 import re
+import itertools
 
-ax_dir_options = ["NEXT","PREVIOUS","ANY"]
-ax_unit_options = ["Character","Format","Word","Line","Paragraph","Page","Document"]
+ax_units = ["Character","Format","Word","Line","Paragraph","Page","Document"]
 
 mod = Module()
 
 mod.list("text_search_direction","directions from cursor in which text can be searched")
+mod.list("text_search_unit","units of text that can be searched for in windows accessibility")
 
-def find_target(target: re.Pattern, 
-                text_range: ax.TextRange = None) -> ax.TextRange:
-    """Searches for the target"""
-    print("FUNCTION: find_target")
+def precise_target_and_position(target: re.Pattern, 
+                text_range: ax.TextRange,
+                search_dir: str = "DOWN",
+                ordinal: int = 1):
+    """Returns a tuple of (str,int) representing the precise match 
+        and the ordinal rank of that precise match from the beginning, 
+        so that we can transfer information from the regex match 
+        to the windows accessibility TextRange.find_text function"""
+    # Note this is not going to work very well for things like "second previous parens"
+
+    # find all instances of the target within the text range text
+    t = text_range.text
+    m = re.findall(target,t)
+    if m and len(m) >= ordinal:
+        # determine precise target and loop parameters to iterate through preceding matches
+        if search_dir.upper() == "UP":
+            precise_trg = m[-ordinal]
+            start,stop,step = len(m) - 1, len(m) - ordinal, -1
+        else:
+            precise_trg = m[ordinal - 1]
+            start,stop,step = 0,ordinal - 1,1
+        # precise_ordinal is index of target within windows accessibility search
+        # i.e. number of times needed to iterate forward or backward
+        precise_ordinal = 1
+        for i in range(start,stop,step):
+            if m[i] == precise_trg:
+                precise_ordinal += 1
+        return (precise_trg,precise_ordinal)
+    else:
+        return (None,None)
+        
+def find_target(trg: re.Pattern, 
+                text_range: ax.TextRange = None,
+                search_dir: str = "DOWN",
+                ordinal: int = 1) -> ax.TextRange:
+    """Searches for the target and return a ax.TextRange word object or None"""
+    # Handle case of no TextRange input
     if text_range is None:
         el = ui.focused_element()
         if "Text" not in el.patterns:
             print("Error in function find_target: focused element does not have text pattern")
-            return 
-        # Get scope as a text range
+            return None
         text_range = el.text_pattern.selection[0].clone()
-    # find target within the text range text
-    t = text_range.text
-    m = re.search(target,t)
-    if m:
-        print("Target found:")
-        print(m.group(0))
-        print(m.groups())
-        r = text_range.find_text(m.group(0))
+    # Use regex to find exact match text and its position
+    precise_trg,precise_ordinal = precise_target_and_position(trg,text_range,search_dir,ordinal)
+    if precise_trg != None:
+        # Iteratively search for precise target using windows accessibility
+        back = search_dir.upper() == "UP"
+        r = text_range.find_text(precise_trg,backward = back)
+        while precise_ordinal > 1:
+            actions.sleep(0.05)
+            precise_ordinal -= 1
+            if search_dir.upper() == "UP":
+                text_range.move_endpoint_by_range("End","Start",target = r.clone())
+            else:
+                text_range.move_endpoint_by_range("Start","End",target = r.clone())
+            r = text_range.find_text(precise_trg,backward = back)            
+            r.select()
         return r
     else:
         print("Target not found :(")
         return None
-def get_scope(scope_dir: str = "ANY",
+
+def get_scope(scope_dir: str = "DOWN",
                 scope_unit: str = "Line",
                 scope_unit_count: int = 7):
     """Returns a text range corresponding to the search scope"""
-    print(f'scope_dir: {scope_dir}')
-    print(f'scope_unit: {scope_unit}')
     # Error Checking
-    if scope_dir not in ax_dir_options:
-        print("Error in function get_scope: scope_dir not in ax_dir_options")
-        return 
-    if scope_unit not in ax_unit_options:
-        print("Error in function get_scope: scope_unit not in ax_unit_options")
+    if scope_unit not in ax_units:
+        print("Error in function get_scope: scope_unit not valid")
         return 
     el = ui.focused_element()
     if "Text" not in el.patterns:
@@ -51,18 +87,91 @@ def get_scope(scope_dir: str = "ANY",
         return 
     # Get scope as a text range
     cur_range = el.text_pattern.selection[0].clone()
-    cur_range.expand_to_enclosing_unit(scope_unit)
-    cur_range.move_endpoint_by_unit("End",scope_unit,scope_unit_count)
-    cur_range.move_endpoint_by_unit("Start",scope_unit,-1*scope_unit_count)
-    # get rid of this later once everything is working:
-#    cur_range.select()
-
+    # avoid selecting anything in current selection
+    if scope_dir.upper() == "UP":
+        cur_range.move_endpoint_by_range("End","Start",target = cur_range.clone())
+    if scope_dir.upper() == "DOWN":
+        cur_range.move_endpoint_by_range("Start","End",target = cur_range.clone())
+    if scope_dir.upper() != "UP":
+        cur_range.move_endpoint_by_unit("End",scope_unit,scope_unit_count)
+    if scope_dir.upper() != "DOWN":
+        cur_range.move_endpoint_by_unit("Start",scope_unit,-1*scope_unit_count)
     return cur_range
-
 
 @mod.action_class
 class Actions:
-    def ax_test(target: re.Pattern):
-        """test"""
-        r = find_target(target,get_scope())
+    def select_text(target: re.Pattern, scope_dir: str = "DOWN", ordinal: int = 1):
+        """Selects text using windows accessibility pattern if possible"""
+        print(f'scope_dir: {scope_dir}')
+        el = ui.focused_element()
+        if "Text" in el.patterns:
+            r = find_target(target,get_scope(scope_dir),search_dir = scope_dir,ordinal = ordinal)
+            if r != None:
+                r.select()
+        else:
+            actions.user.navigation("SELECT",scope_dir,"DEFAULT","default",target,1)
+
+    def go_text(trg: re.Pattern, scope_dir: str, before_or_after: str, ordinal: int = 1):
+        """Navigates to text using windows accessibility pattern if possible"""
+        el = ui.focused_element()
+        if "Text" in el.patterns:
+            r = find_target(trg,get_scope(scope_dir),search_dir = scope_dir,ordinal = ordinal)
+            if r != None:
+                src_pos = "End" if before_or_after.upper() == "BEFORE" else "Start"
+                trg_pos = "Start" if before_or_after.upper() == "BEFORE" else "End"
+                r.move_endpoint_by_range(src_pos,trg_pos,target = r.clone())
+                r.select()
+        else:
+        	actions.user.navigation("GO",scope_dir,"DEFAULT",before_or_after,trg,ordinal)
+
+    def extend_selection(trg: re.Pattern,
+                        scope_dir: str,
+                        before_or_after: str,
+                        ordinal: int = 1):
+        """Extend currently selected text using windows accessibility pattern if possible"""
+        el = ui.focused_element()
+        if "Text" in el.patterns:
+            cur_range = el.text_pattern.selection[0].clone()
+            r = find_target(trg,get_scope(scope_dir),search_dir = scope_dir,ordinal = ordinal)
+            if r != None:
+                src_pos = "Start" if scope_dir.upper() == "UP" else "End"
+                trg_pos = "Start" if before_or_after.upper() == "BEFORE" else "End"
+                cur_range.move_endpoint_by_range(src_pos,trg_pos,target = r.clone())
+                cur_range.select()
+        else:
+            actions.user.navigation("EXTEND",scope_dir,"DEFAULT",before_or_after,trg,ordinal)
+
+    def move_by_unit(unit: str, scope_dir: str, ordinal: int = 1):
+        """Moves the cursor by the selected number of units"""
+        el = ui.focused_element()
+        if "Text" in el.patterns:
+            cur_range = el.text_pattern.selection[0].clone()
+            ordinal = -ordinal if scope_dir.upper() == "UP" else ordinal
+            cur_range.move(unit,ordinal)
+            
+            cur_range.select()
+            cur_range.scroll_into_view(True)
+            
+
+    def select_unit(unit: str):
+        """Selects the enclosing unit around the current cursor position"""
+        el = ui.focused_element() 
+        if "Text" in el.patterns:
+            cur_range = el.text_pattern.selection[0].clone()
+            cur_range.expand_to_enclosing_unit(unit)
+            cur_range.select()
+        else:
+            actions.user.edit_command("select", unit)
+                
+    def test_backward_search():
+        """for debugging"""
+        el = ui.focused_element()
+        cur_range = el.text_pattern.selection[0].clone()
+        r = cur_range.find_text("distance",backward = True)
+        r.select()
+    def test_forward_search():
+        """for debugging"""
+        el = ui.focused_element()
+        cur_range = el.text_pattern.selection[0].clone()
+        r = cur_range.find_text("distance",backward = False)
         r.select()
--- windows accessibility/text selection/text selection.talon
+++ windows accessibility/text selection/text selection.talon
@@ -1,3 +1,25 @@
 os: windows
 -
-test selection: user.find_target("test")
+test backward search: user.test_backward_search()
+test forward search: user.test_forward_search()
+
+# SELECTION
+
+select [<user.ordinals>] {user.text_search_direction} <user.navigation_target>: user.select_text(navigation_target,text_search_direction,ordinals or 1)
+	# example spoken forms:
+	#   select next hippopotamus
+	#   select third previous brief exponent
+
+extend {user.before_or_after} [<user.ordinals>] {user.text_search_direction} <user.navigation_target>:
+	user.extend_selection(navigation_target,text_search_direction,before_or_after,ordinals or 1)
+	# examples spoken forms:
+	#   extend after next hippopotamus
+	#   extend before third to previous brief exponent
+
+go {user.before_or_after} [<user.ordinals>] {user.text_search_direction} <user.navigation_target>:
+	user.go_text(navigation_target,text_search_direction,before_or_after,ordinals or 1)
+
+{user.text_search_direction} <number_small> {user.text_search_unit}: 
+	user.move_by_unit(text_search_unit,text_search_direction,number_small)
+
+#select {user.text_search_unit}: user.select_unit(text_search_unit)
