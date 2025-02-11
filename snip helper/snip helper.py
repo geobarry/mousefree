22bb4b38dbd19dcd84cc016293b1111ddae2c335
Barry Kronenfeld <barrykronenfeld@gmail.com>
2025-02-11 00:15:59 -0600

    got snip helper basic functionality working

--- snip helper/snip helper.py
+++ snip helper/snip helper.py
@@ -1,25 +1,62 @@
 from talon import Module, Context, clip, ctrl, cron, actions, canvas, screen, settings
 from talon.windows import ax as ax, ui as winui
-from talon.types import Point2d as Point2d, rect as rect
+from talon.types import Point2d, Rect
 from talon.skia import  Paint
 from typing import Callable
+
+saved_rect = []
+last_mouse = None
 
 mod = Module()
 
 @mod.action_class
 class Actions:
-    def snip_rect(rect: rect):
+    def snip_rect(rect: Rect):
         """performs a screen capture of given rectangle and copies to clipboard"""
-        ctrl.mouse_move(rect.x+20,rect.y+20)
+        ctrl.mouse_move(rect.x,rect.y)
         actions.key("super-shift-s")
-        actions.sleep(1.5)
-        #actions.mouse_click()
-        #actions.sleep(1.5)
+        actions.sleep(1.0)
         actions.user.mouse_drag(0)
-        # actions.sleep(1.5)
-        actions.user.slow_mouse(rect.x + rect.width-20,rect.y + rect.height-20, ms=3000)
-        actions.mouse_release(0)
+        actions.user.slow_mouse(rect.x + rect.width,rect.y + rect.height, ms=300, callback = lambda: actions.mouse_release(0))
     def snip_screen():
         """copies current screen capture to clipboard"""
-        # move mouse to upper left
         actions.user.snip_rect(winui.main_screen().rect)
+    def snip_window():
+        """copies current window capture to clipboard"""
+        actions.user.snip_rect(winui.active_window().rect)
+    def save_rect(pt1: tuple = None, pt2: tuple = None, pos: int = -1):
+        """records a rectangle for snipping"""
+        # get first point
+        if pt1 == None:
+            pt1 = ctrl.mouse_pos()
+        # get second point
+        if pt2 == None:
+            global last_mouse
+            pt2 = last_mouse
+            if pt2 == None:
+                exit
+        # construct rectangle
+        xmin = min(pt1[0],pt2[0])
+        ymin = min(pt1[1],pt2[1])
+        xmax = max(pt1[0],pt2[0])
+        ymax = max(pt1[1],pt2[1])
+        rect = Rect(xmin,ymin,xmax - xmin,ymax - ymin)
+        # manage rectangles list
+        if pos < 0 or pos > len(saved_rect) - 1:
+            saved_rect.append(rect)
+        else:
+            saved_rect[pos] = rect
+        print(f"FUNCTION save_rect saved_rect:\n{saved_rect}")
+    def start_rect():
+        """Records current mouse position as start of rectangle"""
+        global last_mouse
+        last_mouse = ctrl.mouse_pos()
+    def snip_saved_rect(pos: int = 0):
+        """Captures screen for designated rectangle"""
+        if -1 < pos < len(saved_rect):
+            actions.user.snip_rect(saved_rect[pos])
+    def clear_snip_rect():
+        """clears the list of saved snip rectangles"""
+        global saved_rect
+        saved_rect = []
+            
--- snip helper/snip helper.talon
+++ snip helper/snip helper.talon
@@ -1,3 +1,8 @@
 os: windows
 -
-snip full screen$: user.snip_screen()
+snip screen$: user.snip_screen()
+snip window$: user.snip_window()
+snip rectangle [start]$: user.start_rect()
+snip rectangle finish$: user.save_rect()
+snip rectangles clear: user.clear_snip_rect()
+snip <number>: user.snip_saved_rect(number-1)
